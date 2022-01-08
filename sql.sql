7b9555664af574db76f0e4f022c61a89e1a96bcb
Hoàng Đức <hoangduc.dtdl@gmail.com>
2022-01-08 17:15:45 +0700

    Real life parking update

---
+++ sql.sql
@@ -0,0 +1,8 @@
+ALTER TABLE `player_vehicles` ADD COLUMN `modifications` text NOT NULL;
+ALTER TABLE `player_vehicles` ADD COLUMN `posX` float NOT NULL;
+ALTER TABLE `player_vehicles` ADD COLUMN `posY` float NOT NULL;
+ALTER TABLE `player_vehicles` ADD COLUMN `posZ` float NOT NULL;
+ALTER TABLE `player_vehicles` ADD COLUMN `rotX` float NOT NULL;
+ALTER TABLE `player_vehicles` ADD COLUMN `rotY` float NOT NULL;
+ALTER TABLE `player_vehicles` ADD COLUMN `rotZ` float NOT NULL;
+ALTER TABLE `player_vehicles` ADD COLUMN `lastUpdate` int(11) NOT NULL DEFAULT '0';
