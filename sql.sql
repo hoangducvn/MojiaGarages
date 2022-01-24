51ceee4d29c190a22f9050360f8645e9273b807d
BlueSky <80676050+BlueSky-fur@users.noreply.github.com>
2022-01-24 04:36:13 +0100

    Update sql.sql
    This fixes the ReallifeParking issue where you buy cars but dont get them added to the DB.
    
    Tested by myself

--- sql.sql
+++ sql.sql
@@ -1,7 +1,7 @@
-ALTER TABLE `player_vehicles` ADD COLUMN `posX` float NOT NULL;
-ALTER TABLE `player_vehicles` ADD COLUMN `posY` float NOT NULL;
-ALTER TABLE `player_vehicles` ADD COLUMN `posZ` float NOT NULL;
-ALTER TABLE `player_vehicles` ADD COLUMN `rotX` float NOT NULL;
-ALTER TABLE `player_vehicles` ADD COLUMN `rotY` float NOT NULL;
-ALTER TABLE `player_vehicles` ADD COLUMN `rotZ` float NOT NULL;
+ALTER TABLE `player_vehicles` ADD COLUMN `posX` float NOT NULL DEFAULT '0';
+ALTER TABLE `player_vehicles` ADD COLUMN `posY` float NOT NULL DEFAULT '0';
+ALTER TABLE `player_vehicles` ADD COLUMN `posZ` float NOT NULL DEFAULT '0';
+ALTER TABLE `player_vehicles` ADD COLUMN `rotX` float NOT NULL DEFAULT '0';
+ALTER TABLE `player_vehicles` ADD COLUMN `rotY` float NOT NULL DEFAULT '0';
+ALTER TABLE `player_vehicles` ADD COLUMN `rotZ` float NOT NULL DEFAULT '0';
 ALTER TABLE `player_vehicles` ADD COLUMN `lastUpdate` int(11) NOT NULL DEFAULT '0';
